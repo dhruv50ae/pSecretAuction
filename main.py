f5ca094786e7fdd194b25c9a1050fd3f50c1f18f
dhruv50ae <linkdp2012@gmail.com>
2022-04-12 12:47:09 +0100

    Endgame Commit

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-travelLog = [
-    {
-        "country": "France", "visit": 12, "cities": ["Paris", "Lille", "Dijon"]
-    },
-    {
-        "country": "Germany", "visit": 5, "cities": ["Berlin", "Hamburg", "Stuttgart"]
-    }
-]
+
+from turtle import clear
 
 
-def addNewCountry(countryVisited, timesVisited, citiesVisited):
-    newCountry = {}
-    newCountry["country"] = countryVisited
-    newCountry["visits"] = timesVisited
-    newCountry["cities"] = citiesVisited
-    travelLog.append(newCountry)
+bids = {}
+biddingFinished = False
 
 
-addNewCountry("Russia", 2, ["Moscow", "Saint Petersburg"])
-print(travelLog)
+def findHighestBidder(biddingRecord):
+    highestBid = 0
+    winner = ""
+    for bidder in biddingRecord:
+        bidAmount = biddingRecord[bidder]
+        if bidAmount > highestBid:
+            highestBid = bidAmount
+            winner = bidder
+    print(f"The winner is {winner} at ${highestBid}!")
+
+
+while not biddingFinished:
+    name = input("What is your name? : ")
+    price = int(input("What is your bid? $"))
+    bids[name] = price
+    shouldContinue = input("Are there any other bidders? Type 'yes' or 'no' ")
+    if shouldContinue == 'no':
+        biddingFinished = True
+        findHighestBidder(bids)
+    elif shouldContinue == 'yes':
+        clear()
